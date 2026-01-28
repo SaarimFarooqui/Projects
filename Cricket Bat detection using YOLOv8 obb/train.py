fc5915015b47c2627c74033dedc442c98e2b59a3
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-01-28 12:45:45 +0500

    Yolo V8 model training code

---
+++ Cricket Bat detection using YOLOv8 obb/train.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8s-obb.pt")
+
+model.train(
+    task = "obb",
+    data = "dataset\data.yaml",
+    epochs = 100,
+    imgsz = 640,
+    batch = 16,
+    device = 0,
+    name = "bestBat_OBB",
+    project = "result"
+    save = true
+)
