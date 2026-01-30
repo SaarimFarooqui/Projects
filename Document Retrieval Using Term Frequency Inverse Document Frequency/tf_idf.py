32b9bdb1fc61b6b4b5da71caf33654f3096db2ae
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-01-30 22:22:13 +0500

    Created a separate function for the pre-processed words of the merged single document

--- Document Retrieval Using Term Frequency Inverse Document Frequency/tf_idf.py
+++ Document Retrieval Using Term Frequency Inverse Document Frequency/tf_idf.py
@@ -9,17 +9,21 @@ document4 = "I am afraid gaming might affect my academic performace"
 
 documents = [document1, document2, document3]
 
-def tf_idf(documents):
+#The function get_all_words() takes list of all documents, tokenizes all words, removes stop words and non-alpha numeric words and returns a list of those pre-processed words
+
+def get_all_words(documents): 
   merged_documents = ""
   for document in documents:
     document = document.lower() + " "
     merged_documents += document 
-  
-  stop_words = set(stopwords.words('english')) #Storing stopwords in this variable
-  
-  list_of_words = [ word for word in word_tokenize(merged_documents) if word not in stop_words and word.isalnum() ]
-  
+  #stopwords stored in stop_words
+  stop_words = set(stopwords.words('english')) 
+  list_of_words = [ word for word in word_tokenize(merged_documents) if word not in stop_words and word.isalnum()]
   return list_of_words
+  
+
+def tf_idf(documents):
+  l = get_all_words(documents)
 
 result = tf_idf(documents)
 
