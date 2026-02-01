e8a28e84715b8d23498b36f9c2343925b3313dd5
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-02-01 13:04:56 +0500

    THe frequency table's column headings are successfully generated and first column is filled with the names of unique words. Rest columns are all 0s. Creating a separate function to calculate word frequencies of each document with respect to a word. Not completed yet

--- Document Retrieval Using Term Frequency Inverse Document Frequency/tf_idf.py
+++ Document Retrieval Using Term Frequency Inverse Document Frequency/tf_idf.py
@@ -1,20 +1,23 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from collections import defaultdict
 
 stop_words = set(stopwords.words('english')) 
 
-# row_of_words returns a list of words in lower case which are alpha-numeric and not included in stop words.
+#row_of_words returns a list of words in lower case which are alpha-numeric and not included in stop words.
 def row_of_words(single_document):
   words = [word.lower() for word in word_tokenize(single_document) if word not in stop_words and word.isalnum()]
   return words
 
-def table_of_words(documents):
-  table_of_elements = []
-  for document in documents:
-    table_of_elements.append(row_of_words(document))
-  return table_of_elements
 
-# unique_words is a function that extracts unique words from table and returns a list
+#The function table_of_words uses above function row_of_words to generate lists of  pre-processed words of each document. Each list contains preprocessed words from each document,  means no of lists = no of documents. These lists are the appended into another list which results into a lists within a list. Which serves a table.
+def table_of_words(documents):
+  table = []
+  for document in documents:
+    table.append(row_of_words(document))
+  return table
+
+#unique_words is a function that extracts unique words from table and returns a list
 def unique_words(table):
   u_words = []
   for wordlist in table:
@@ -23,6 +26,29 @@ def unique_words(table):
         u_words.append(word)
   return u_words
 
+#This function
+def word_frequency(list_uniquewords, row_words):
+  document_dictionary = defaultdict(int)
+  
+  for i in list_uniquewords:
+    document_dictionary[i] = 0
+        
+  
+  
+def frequency_table(list_documents, list_uniquewords):
+  # The number of documents + 1 = number of columns.
+  # The number of unique words + 1 = number of rows.
+  frequency_table = [[0 for _ in range(len(list_documents)+1)] for _ in range(len(list_uniquewords)+1)]
+  frequency_table[0][0] = "UNIQUE WORDS"
+  
+  for index, word in enumerate(list_uniquewords):
+    frequency_table[index+1][0] = word
+    
+  for index, word in enumerate(list_documents):
+    frequency_table[0][index+1] = f"DOCUMENT {index+1}"
+    
+  return frequency_table
+
 document1 = "I have been playing video games since this morning."
 document2 = "My favourite game has always been GTAV. It is an amazing game"
 document3 = "Through online games, people can collectively solve large-scale computational problems."
@@ -30,10 +56,9 @@ document4 = "I am afraid gaming might affect my academic performace"
 documents = [document1, document2, document3, document4]
 
 table = table_of_words(documents)
+uniquewords = unique_words(table)
 
-extracted = unique_words(table)
+freq_table = frequency_table(documents, uniquewords)
 
-empty_table = [[0 for _ in range(len(documents))] for _ in range(len(extracted))]
-
-for i in empty_table:
+for i in freq_table:
   print(i)
