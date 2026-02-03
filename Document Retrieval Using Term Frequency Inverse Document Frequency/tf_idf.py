b9f2d6087cd5c60b8a72baab4aeccb5deded6398
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-02-03 11:49:04 +0500

    Successfully created the function for document frequency. Previously it was for term frequency. Now moving for inverse document frequency

--- Document Retrieval Using Term Frequency Inverse Document Frequency/tf_idf.py
+++ Document Retrieval Using Term Frequency Inverse Document Frequency/tf_idf.py
@@ -1,6 +1,8 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import pandas as pd
+from collections import defaultdict
+
 stop_words = set(stopwords.words('english')) 
 
 #row_of_words returns a list of words in lower case which are alpha-numeric and not included in stop words.
@@ -24,11 +26,11 @@ def unique_words(table):
         u_words.append(word)
   return u_words
   
-def frequency_table(list_documents, list_uniquewords, table_of_words):
+def term_frequency_table(list_documents, list_uniquewords, table_of_words):
   # The number of documents + 1 = number of columns.
   # The number of unique words + 1 = number of rows.
   frequency_table = [[0 for _ in range(len(list_documents)+1)] for _ in range(len(list_uniquewords)+1)]
-  frequency_table[0][0] = "UNIQUE WORDS"
+  frequency_table[0][0] = "TERM"
   
   for index, word in enumerate(list_uniquewords):
     frequency_table[index+1][0] = word
@@ -42,6 +44,17 @@ def frequency_table(list_documents, list_uniquewords, table_of_words):
       
   return frequency_table
 
+def document_frequency_table(table):
+  doc_freq_dict = defaultdict(int)
+  merged = []
+  for row in table:
+    merged += row
+  for word in merged:
+    doc_freq_dict[word] += 1
+  return doc_freq_dict
+  
+  
+
 document1 = "I have been playing video games since this morning."
 document2 = "My favourite game has always been GTAV. It is an amazing game"
 document3 = "Through online games, people can collectively solve large-scale computational problems."
@@ -50,7 +63,8 @@ documents = [document1, document2, document3, document4]
 
 table = table_of_words(documents)
 uniquewords = unique_words(table)
-freq_table = frequency_table(documents, uniquewords, table)
+freq_table = term_frequency_table(documents, uniquewords, table)
 
-df = pd.DataFrame(freq_table)
-print(df)
+doc_freq = document_frequency_table(table)
+for key, value in doc_freq.items():
+  print(f"{key}: {value}")
