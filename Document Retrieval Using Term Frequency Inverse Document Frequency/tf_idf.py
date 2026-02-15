c11362e87fdfa4147816d7a3cc0d977782bb1860
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-02-15 12:43:22 +0500

    Successfully retrieving documents using the tf-idf method

--- Document Retrieval Using Term Frequency Inverse Document Frequency/tf_idf.py
+++ Document Retrieval Using Term Frequency Inverse Document Frequency/tf_idf.py
@@ -26,7 +26,6 @@ def unique_words(table):
         u_words.append(word)
   return u_words
 
-
 def TermFrequencyLists(uniquewords, table):
     TermFrequencyLists = []
     TermFrequencyLists.append(uniquewords)
@@ -34,7 +33,7 @@ def TermFrequencyLists(uniquewords, table):
     for row in table:
         TermFrequencyList = []
         for word in uniquewords:
-            TermFrequencyList.append(round(row.count(word)/len(row), 3))
+            TermFrequencyList.append(round(row.count(word)/len(row), 4))
         TermFrequencyLists.append(TermFrequencyList)
     
     return TermFrequencyLists
@@ -52,7 +51,7 @@ def InverseDocumentFrequency(documentfrequency, documents):
   N = len(documents)
   
   for key, value in documentfrequency.items():
-    inversedocumentfrequency[key] = round(math.log(N/value), 3)
+    inversedocumentfrequency[key] = round(math.log(N/value), 4)
   
   return inversedocumentfrequency
 
@@ -65,14 +64,12 @@ def TF_IDF(termfrequency, inversedocumentfrequency):
     tfidfRow = []
     if rowNum != 0:
       for index, value in enumerate(row):
-        tfidfRow.append(round(value * idfvalues[index],3))
+        tfidfRow.append(round(value * idfvalues[index],4))
       tfidf.append(tfidfRow)
     
   return tfidf
 
 def InputVector(input, uniquewords, inversedocumentfrequency):
-  InputVectorwithHeadings = []
-  InputVectorwithHeadings.append(uniquewords)
   vector = []
   inputWordFrequency = defaultdict(float)
   preprocessedRow = row_of_words(input)
@@ -84,15 +81,37 @@ def InputVector(input, uniquewords, inversedocumentfrequency):
   inputWordFrequencies = list(inputWordFrequency.values())
   
   for index, value in enumerate(idfvalues):
-    vector.append(round(idfvalues[index] * inputWordFrequencies[index],3))
-  InputVectorwithHeadings.append(vector)
-  
-  return InputVectorwithHeadings
+    vector.append(round(idfvalues[index] * inputWordFrequencies[index],4))
+  return vector
     
-  
-document1 = "data science machine"
-document2 = "data science"
-document3 = "machine learning"
+def DotProduct(vector1, vector2):
+  DotProduct = 0
+  for i in range(len(vector1)):
+    DotProduct += vector1[i] * vector2[i]
+  return round(DotProduct,4)
+
+def Magnitude(vector):
+  Magnitude= 0
+  for value in vector:
+    Magnitude += value ** 2
+  return round(Magnitude ** 0.5, 4)
+
+def CosineSimilarityRanking(inputvector, tfidf):
+  numerator = 0
+  denomerator = 0
+  CosineSimilarityDictionary = defaultdict(float)
+  tfidf.pop(0)
+  for index, row in enumerate(tfidf):
+    numerator = DotProduct(inputvector, row)
+    denomerator = Magnitude(inputvector) * Magnitude(row)
+    CosineSimilarityDictionary[f"Document: {index+1}"] = round (numerator / denomerator, 4)
+    sorted_dict = dict(sorted(CosineSimilarityDictionary.items(), key=lambda x: x[1], reverse=True))
+  return sorted_dict
+
+document1 = "What is the future of Data Science"
+document2 = "How can you use machine learning in personal life"
+document3 = "Is this a good time to start learning data science and machine learning"
+input = "Learning data science"
 documents = [document1, document2, document3]
 
 table = table_of_words(documents)
@@ -100,7 +119,17 @@ uniquewords = unique_words(table)
 termfrequency = TermFrequencyLists(uniquewords, table)
 documentfrequency =  DocumentFrequency(uniquewords, table)
 inversedocumentfrequency = InverseDocumentFrequency(documentfrequency, documents)
-tfidf = TF_IDF(termfrequency, inversedocumentfrequency)
 
-df = pd.DataFrame(tfidf)
-df.to_html("tfldf.html", index = False, header = False)
+tfidf = TF_IDF(termfrequency, inversedocumentfrequency)
+inputvector = InputVector(input, uniquewords, inversedocumentfrequency)
+cosinesimilarity = CosineSimilarityRanking(inputvector, tfidf)
+
+tfidf = pd.DataFrame(tfidf)
+print("TF IDF")
+print(tfidf.to_string(index=False, header=False))
+print("\n")
+print("Input Vector")
+print(inputvector)
+print("\n")
+for key, value in cosinesimilarity.items():
+  print(f"{key} : {value}")
